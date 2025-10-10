ac023dcc721424cc79b1027bc421f13a961efc9d
Zuheb <zuheb.mohamud@georgebrown.ca>
2025-10-10 12:24:57 -0400

    Lab 4 completed

--- COMP2139-ICE/Controllers/ProjectController.cs
+++ COMP2139-ICE/Controllers/ProjectController.cs
@@ -1,62 +1,123 @@
+using Microsoft.AspNetCore.Mvc;
 using COMP2139_ICE.Data;
 using COMP2139_ICE.Models;
-using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-namespace COMP2139_ICE.Controllers
+namespace COMP2139_ICE.Controllers;
+
+public class ProjectController : Controller
 {
-    public class ProjectController : Controller
+    private readonly ApplicationDbContext _context;
+
+    public ProjectController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public ProjectController(ApplicationDbContext context)
+    public IActionResult Index()
+    {
+        var projects = _context.Projects.ToList();
+        return View(projects);
+    }
+    [HttpGet]
+    public IActionResult Create()
+    {
+
+        return View();
+    }
+
+    [HttpPost]
+    [AutoValidateAntiforgeryToken]
+    public IActionResult Create(Project project)
+    {
+        if (ModelState.IsValid)
         {
-            _context = context;
+            _context.Add(project);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
-
-        // GET: /Project/
-        [HttpGet]
-        public IActionResult Index()
+        return View(project);
+    }
+    
+    [HttpGet]
+    public IActionResult Details(int id)
+    {
+        var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
+        if (project == null) return NotFound();
+        return View(project);
+    }
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var project = _context.Projects.Find(id);
+        if(project == null)
         {
-            // Get all projects from the database
-            var projects = _context.Projects.ToList();
-            return View(projects);
+            return NotFound();
         }
+        return View(project);
+    } 
+    
+//Lab4 - Part3 - #2 
+    [HttpPost] 
+    [ValidateAntiForgeryToken] 
+    
+    public IActionResult Edit(int id, [Bind("ProjectId","Name","Description")] Project project) 
+    { 
+        if (id != project.ProjectId) 
+        { 
+            return NotFound(); 
+        } 
+        if (ModelState .IsValid) 
+        { 
+            try 
+            { 
+                _context.Projects.Update(project); 
+                _context.SaveChanges(); 
+            } 
+            catch (DbUpdateConcurrencyException) 
+            { 
+                if (!ProjectExists(project.ProjectId)) 
+                { 
+                    return NotFound(); 
+                } 
+                else 
+                { 
+                    throw; 
+                } 
+            } 
+            return RedirectToAction("Index"); 
+        } 
+        return View(project); 
+    } 
+    private bool ProjectExists(int id) 
+    { 
+        return _context.Projects.Any(e => e.ProjectId == id); 
+    }
 
-        // GET: /Project/Create
-        [HttpGet]
-        public IActionResult Create()
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
+        if (project == null)
         {
-            return View();
+            return NotFound();
         }
-
-        // POST: /Project/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Project project)
+        return View(project);
+    }
+    
+    [HttpPost, ActionName("DeleteConfirmed")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var project = _context.Projects.Find(id);
+        if (project != null)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Projects.Add(project);
-                _context.SaveChanges();  // save to DB
-                return RedirectToAction(nameof(Index));
-            }
-
-            return View(project);
+            _context.Projects.Remove(project);
+            _context.SaveChanges();
+            return RedirectToActionPermanent("Index");
         }
-
-        // GET: /Project/Details/5
-        [HttpGet]
-        public IActionResult Details(int id)
-        {
-            var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
-
-            if (project == null)
-            {
-                return NotFound();
-            }
-
-            return View(project);
-        }
+        return NotFound();
     }
 }
+
---
+++ COMP2139-ICE/Controllers/ProjectTaskController1.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2139_ICE.Controllers;
+
+public class ProjectTaskController1 : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
